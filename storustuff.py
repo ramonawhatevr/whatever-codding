cec4405adb7b127c1d414f6eb2245285441584d8
ramonawhatevr <ulis.7891@gmail.com>
2025-11-11 16:34:25 -0500

    Add files via upload

---
+++ storustuff.py
@@ -0,0 +1,21 @@
+print("Welcome to the disgusting magical forest!")
+
+print("You are on a path, but it diverges at some point. What will you do?")
+
+input("left or right?")
+
+x = input()
+
+
+if x == "left":
+    print("you reached a swamp! it stinks")
+
+elif x == "right":
+    print("you reached a garden! bugs are everywhere")
+    
+if "right" == print("you reached a garden! bugs are everywhere"):
+    print("you see two doors to get into a castle! door one or two?")
+    
+elif "left" == print("you reached a swap! it stinks"):
+    print("you see two dirt paths in front! path one or two?")
+    
